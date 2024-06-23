2a00f867eb74c8f67653f5a7a9039de80f84e094
Benjamin Prud'homme <bprudhomme@gradcenter.cuny.edu>
2024-06-23 19:08:29 -0400

    Revert "Update JdkFutureAdapters.java"
    This reverts commit 31aea8de4dfdac9ecf7d60faa190c3621cb1e5b4.

--- guava/src/com/google/common/util/concurrent/JdkFutureAdapters.java
+++ guava/src/com/google/common/util/concurrent/JdkFutureAdapters.java
@@ -21,7 +21,6 @@ import com.google.common.annotations.GwtIncompatible;
 import com.google.common.annotations.J2ktIncompatible;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -109,7 +108,7 @@ public final class JdkFutureAdapters {
             .setDaemon(true)
             .setNameFormat("ListenableFutureAdapter-thread-%d")
             .build();
-    private static final ExecutorService defaultAdapterExecutor =
+    private static final Executor defaultAdapterExecutor =
         Executors.newVirtualThreadPerTaskExecutor();
 
     private final Executor adapterExecutor;
